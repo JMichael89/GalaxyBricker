041c4486722e849bffccf0afe8243d47f0b37525
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2023-05-18 18:40:15 -0300

    Update Colider

--- scripts/GameEngine.py
+++ scripts/GameEngine.py
@@ -2,7 +2,6 @@ from random import random, randrange
 
 from scripts.characters.Ball import Ball, BallType
 from scripts.characters.Block import Block, BlockType
-from scripts.characters.Platform import Platform, PlatformType
 from scripts.models.Collider import check_collision, check_external_collider
 from scripts.utils.Vector import Vector
 from scripts.utils.Window import Window
@@ -15,21 +14,24 @@ class GameEngine:
     def __init__(self):
         self.pygame = pygame
         self.pygame.init()
-        self.windows = Window(pygame, "Galaxy Bricker", Dimension(400, 400))
+        self.windows = Window(pygame, "Galaxy Bricker", Dimension(1200, 700))
 
     def run(self):
 
         tests = "true"
-        ball_test = Ball(dimension=Dimension(20, 20))
+        ball_test = Ball(dimension=Dimension(100, 100))
         ball_test.select_ball(BallType.basic_white)
+        ball_test.set_speed(1)
+
         block_test = Block()
         block_test.select_bloc(BlockType.b1)
 
         # Tests A2
-        ball_test.set_position(0, 0)
-        ball_test.set_speed(random(), random())
+        ball_test.set_position(100, 100)
+        ball_test.set_position(340, 200)
+        ball_test.set_direction(0, -1)
 
-        block_test = Block(Vector(100, 100), Dimension(200, 200))
+        block_test = Block(Vector(400, 0), Dimension(200, 50))
         block_test.select_bloc(BlockType.b1)
 
         self.windows.add_element(ball_test, block_test)
@@ -47,10 +49,12 @@ class GameEngine:
                     for ball in [ball_test]:
                         if check_collision(block, ball):
                             if check_external_collider(block, ball):
-                                #tests = "break"
-                                #ball.speed.multiplication(0)
+                                #print("Break!!!")
                                 #print("ball  :", ball.position)
                                 #print("block :", block.position)
+
+                                #tests = "break"
+                                #ball.speed.multiplication(0)
                                 ...
 
             update(self.windows, ball_test, block_test)
@@ -61,11 +65,11 @@ class GameEngine:
 def update(windows, *elements):
     for element in elements:
         if (element.position.x + element.dimension.x) >= windows.dimension.x:
-            element.speed.x *= -1
+            element.direction.x *= -1
         elif element.position.x < 0:
-            element.speed.x *= -1
+            element.direction.x *= -1
 
         if (element.position.y + element.dimension.y) >= windows.dimension.y:
-            element.speed.y *= -1
+            element.direction.y *= -1
         elif element.position.y < 0:
-            element.speed.y *= -1
+            element.direction.y *= -1
--- scripts/models/Collider.py
+++ scripts/models/Collider.py
@@ -28,21 +28,111 @@ def check_external_collider(bloc: Element, ball: Ball):
 
         elif bloc.position.y + bloc.get_height() < ball.get_center().y <= bloc.position.y + bloc.get_height() + ball.get_radius():
             intersection_area.append("Down")
-            print(intersection_area)
 
-    if intersection_area.__len__() == 2:
-        tratar_colisao_quina_bloco(bloc, ball, intersection_area)
-        ...
+    if intersection_area.__len__() == 1:
+        collider = intersection_area[0]
+        print(collider)
 
-    elif intersection_area.__len__() == 1:
-        if intersection_area[0] == "Top" or intersection_area[0] == "Down":
-            ball.speed.y *= -1
-        if intersection_area[0] == "Left" or intersection_area[0] == "Right":
-            ball.speed.x *= -1
+        if collider == "Top" or collider == "Down":
+            ball.direction.y *= -1
+        if collider == "Left" or collider == "Right":
+            ball.direction.x *= -1
 
-    return intersection_area
+        return collider
+
+    elif intersection_area.__len__() == 2:
+        collider = collide_corner(bloc, ball, intersection_area)
+
+        if collider:
+            print(collider)
+            return collider
 
 
-def tratar_colisao_quina_bloco(bloc: Element, ball: Ball, intersection_area):
-    ...
+def collide_corner(bloc: Element, ball: Ball, intersection_area):
+    collider = None
+    cornner = None
+    cornner_centerball = None
 
+    if intersection_area[1] == "Down":
+        if intersection_area[0] == "Right":
+            cornner = Vector(bloc.position.x + bloc.get_width(), bloc.position.y + bloc.get_height())
+            cornner_centerball = distance_between(cornner, ball.get_center())
+
+            if ball.get_radius() >= cornner_centerball:
+                collider = "Down/Right"
+
+        else:
+            cornner = Vector(bloc.position.x, bloc.position.y + bloc.get_height())
+            cornner_centerball = distance_between(cornner, ball.get_center())
+
+            if ball.get_radius() >= cornner_centerball:
+                collider = "Down/Left"
+
+    elif intersection_area[1] == "Top":
+        if intersection_area[0] == "Right":
+            cornner = Vector(bloc.position.x + bloc.get_width(), bloc.position.y)
+            cornner_centerball = distance_between(cornner, ball.get_center())
+
+            if ball.get_radius() >= cornner_centerball:
+                collider = "Top/Right"
+
+        else:
+            cornner = Vector(bloc.position.x, bloc.position.y)
+            cornner_centerball = distance_between(cornner, ball.get_center())
+
+            if ball.get_radius() >= cornner_centerball:
+                collider = "Top/Left"
+
+    if collider:
+        print(collider)
+        test(ball, cornner)
+        return collider
+
+
+def distance_between(point1: Vector, point2: Vector):
+    x1 = point1.x
+    y1 = point1.y
+    x2 = point2.x
+    y2 = point2.y
+
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
+
+def alterar_direcao_velocidade_bola(corner: Vector, ball_center: Vector, ball_speed: Vector, d):
+    quina_x = corner.x
+    quina_y = corner.y
+    bola_centro_x = ball_center.x
+    bola_centro_y = ball_center.y
+    velocidade_x = ball_speed.x
+    velocidade_y = ball_speed.y
+
+    vetor_quina = (quina_x - bola_centro_x, quina_y - bola_centro_y)
+    comprimento_vetor_quina = d
+    direcao_quina = (vetor_quina[0] / comprimento_vetor_quina, vetor_quina[1] / comprimento_vetor_quina)
+
+    produto_escalar = direcao_quina[0] * velocidade_x + direcao_quina[1] * velocidade_y
+
+    reflexao_x = -1 * produto_escalar * direcao_quina[0] - velocidade_x
+    reflexao_y = -1 * produto_escalar * direcao_quina[1] - velocidade_y
+    reflexao = Vector(reflexao_x, reflexao_y)
+
+    return reflexao
+
+
+def test(ball: Ball, cornner: Vector):
+    print("Tests:")
+    print("cornner          :", cornner)
+    print("ball.center      :", ball.get_center())
+    print("ball.radius      :", ball.get_radius())
+
+    direction_x = (ball.get_center().x - cornner.x)/ball.get_radius()
+    direction_y = (ball.get_center().y - cornner.y)/ball.get_radius()
+    direction = Vector(direction_x, direction_y)
+
+    print("direction_x         :", direction_x)
+    print("direction_y         :", direction_y)
+    print("ball.speed          :", ball.speed)
+    print("direction           :", direction)
+
+    ball.direction = direction
